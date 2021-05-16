8dcdda0596251ee15c10ec6356314f3e877e8655
hecomi <info@hecomi.com>
2021-05-17 00:43:21 +0900

    update bashrc

---
+++ .bash/colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://qiita.com/exy81/items/d58a0c6cdea46ffb36b9
+# reference: man console_codes
+
+for i in `seq 30 38` `seq 40 47` ; do
+    for j in 0 1 2 4 5 7 ; do
+        printf "\033[${j};${i}m"
+        printf "${j};${i}"
+        printf "\033[00m"  # set default
+        printf " "
+    done
+    printf "\n"
+done
--- .bashrc
+++ .bashrc
@@ -20,3 +20,5 @@ export PROMPT_COMMAND="export PROMPT_COMMAND=echo"
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+
+source ~/.bashrc.local
